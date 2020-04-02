354a0b703c284ebf897eb41bc1f3a5a19d8e813f
carlos diaz <eduarbboy12@gmail.com>
2020-04-02 13:12:36 -0500

    Api rest reactor

--- app/src/main/java/com/springboot/webflux/app/controllers/ProductoController.java
+++ app/src/main/java/com/springboot/webflux/app/controllers/ProductoController.java
@@ -7,6 +7,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,6 +23,9 @@ import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
 import java.io.File;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.Duration;
 import java.util.Date;
 import java.util.UUID;
@@ -38,6 +45,18 @@ public class ProductoController {
     @ModelAttribute("categorias")
     public Flux<Categoria> categorias() {
         return productoServices.findAllCategoria();
+    }
+
+    @GetMapping("/upload/img/{nombrefoto:.+}")
+    public Mono<ResponseEntity<Resource>> verFoto(@PathVariable String nombrefoto) throws MalformedURLException {
+        Path ruta = Paths.get(path).resolve(nombrefoto).toAbsolutePath();
+        Resource imagen = new UrlResource(ruta.toUri());
+
+        return Mono.just(
+                ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + imagen.getFilename() + "\"")
+                .body(imagen)
+        );
     }
 
     @GetMapping("/ver/{id}")
