b7e8fcfd01093b7f029854991c18b32c57172b5b
carlos diaz <eduarbboy12@gmail.com>
2020-04-13 15:13:02 -0500

    nuevo proyecto

--- api-rest/src/test/java/com/springboot/reactor/apirest/apirest/ApiRestApplicationTests.java
+++ api-rest/src/test/java/com/springboot/reactor/apirest/apirest/ApiRestApplicationTests.java
@@ -1,17 +1,22 @@
 package com.springboot.reactor.apirest.apirest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.springboot.reactor.apirest.apirest.models.documents.Categoria;
 import com.springboot.reactor.apirest.apirest.models.documents.Producto;
 import com.springboot.reactor.apirest.apirest.models.services.ProductoServices;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import reactor.core.publisher.Mono;
 
 import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -23,10 +28,13 @@ class ApiRestApplicationTests {
 	@Autowired
 	private ProductoServices services;
 
+	@Value(("${config.base.endpoint}"))
+    private String Url;
+
 	@Test
 	void listarTest() {
 		client.get()
-				.uri("/api/v2/productos")
+				.uri(Url)
 				.accept(MediaType.APPLICATION_JSON)
 				.exchange()
 				.expectStatus()
@@ -36,10 +44,12 @@ class ApiRestApplicationTests {
 				.hasSize(9);
 	}
 
+	/*
+	* esta prueba solo funciona cuando se cambia la ruta de la varible url - cambio del api
 	@Test
 	void listarTest2() {
 		client.get()
-				.uri("/api/v2/productos")
+				.uri(Url)
 				.accept(MediaType.APPLICATION_JSON)
 				.exchange()
 				.expectStatus()
@@ -55,13 +65,15 @@ class ApiRestApplicationTests {
 			Assertions.assertThat(productos.size() == 9).isTrue();
 		});
 	}
+	 */
+
 
     @Test
     void verDetalleTest() {
         Producto producto = services.findByNombre("TV Samsung").block();
 
         client.get()
-                .uri("/api/v2/productos/{id}", Collections.singletonMap("id", producto.getId()))
+                .uri(Url + "/{id}", Collections.singletonMap("id", producto.getId()))
                 .accept(MediaType.APPLICATION_JSON)
                 .exchange()
                 .expectStatus()
@@ -77,7 +89,7 @@ class ApiRestApplicationTests {
         Producto producto = services.findByNombre("TV Samsung").block();
 
         client.get()
-                .uri("/api/v2/productos/{id}", Collections.singletonMap("id", producto.getId()))
+                .uri(Url + "/{id}", Collections.singletonMap("id", producto.getId()))
                 .accept(MediaType.APPLICATION_JSON)
                 .exchange()
                 .expectStatus()
@@ -100,7 +112,7 @@ class ApiRestApplicationTests {
         Producto producto = new Producto("Mesa Comedor", 100000.00, categoria);
 
         client.post()
-                .uri("/api/v2/productos")
+                .uri(Url)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
                 .body(Mono.just(producto), Producto.class)
@@ -114,7 +126,8 @@ class ApiRestApplicationTests {
                 .jsonPath("$.nombre").isEqualTo("Mesa Comedor")
                 .jsonPath("$.categoria.nombre").isEqualTo("Muebles");
     }
-
+/**
+ * esta prueba solo funciona cuando se cambia la ruta de la varible url - cambio del api
     @Test
     void crear2Test() {
 
@@ -123,7 +136,7 @@ class ApiRestApplicationTests {
         Producto producto = new Producto("Mesa Comedor", 100000.00, categoria);
 
         client.post()
-                .uri("/api/v2/productos")
+                .uri(Url)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
                 .body(Mono.just(producto), Producto.class)
@@ -132,13 +145,58 @@ class ApiRestApplicationTests {
                 .isCreated()
                 .expectHeader()
                 .contentType(MediaType.APPLICATION_JSON)
-                .expectBody(Producto.class)
+                .expectBody(new ParameterizedTypeReference<LinkedHashMap<String, Object>>() { })
                 .consumeWith(response -> {
-                    Producto p = response.getResponseBody();
+                    Object o = response.getResponseBody().get("producto");
+                    Producto p = new ObjectMapper().convertValue(o, Producto.class);
                     Assertions.assertThat(p.getId()).isNotEmpty();
                     Assertions.assertThat(p.getNombre()).isEqualTo("Mesa Comedor");
                     Assertions.assertThat(p.getCategoria().getNombre()).isEqualTo("Muebles");
                 });
+    }
+
+ */
+
+    @Test
+    void editarTest() {
+        Producto producto = services.findByNombre("TV Samsung").block();
+        Categoria categoria = services.findCategoriaByNombre("Electronico").block();
+        Producto productoEditado = new Producto("AZUS NoteBook", 900000.00, categoria);
+
+        client.put()
+                .uri(Url + "/{id}", Collections.singletonMap("id", producto.getId()))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(Mono.just(productoEditado), Producto.class)
+                .exchange()
+                .expectStatus()
+                .isCreated()
+                .expectHeader()
+                .contentType(MediaType.APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.id").isNotEmpty()
+                .jsonPath("$.nombre").isEqualTo("AZUS NoteBook")
+                .jsonPath("$.categoria.nombre").isEqualTo("Electronico");
+    }
+
+    @Test
+    void eliminarTest() {
+        Producto producto = services.findByNombre("APPLE LAPTOP").block();
+        client.delete()
+                .uri(Url + "/{id}", Collections.singletonMap("id", producto.getId()))
+                .exchange()
+                .expectStatus()
+                .isNoContent()
+                .expectBody()
+                .isEmpty();
+
+        client.get()
+                .uri(Url + "/{id}", Collections.singletonMap("id", producto.getId()))
+                .exchange()
+                .expectStatus()
+                .isNotFound()
+                .expectBody()
+                .isEmpty();
     }
 
 }
---
+++ springboot-client/src/main/java/com/springboot/reactor/client/springbootclient/SpringbootClientApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.reactor.client.springbootclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootClientApplication.class, args);
+	}
+
+}
